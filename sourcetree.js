5b9500b9fd6c53dd369e17d064cd33737743373d
AA <>
2024-07-25 11:27:40 -0600

    Primer commit que incluye html, css y js.

---
+++ sourcetree.js
@@ -0,0 +1,11 @@
+document.getElementById('purposeBtn').addEventListener('click', function() {
+    document.getElementById('infoText').innerHTML = 'SourceTree ayuda a visualizar las ramificaciones del código, cambios y contribuciones de diferentes desarrolladores en proyectos colaborativos.';
+});
+
+document.getElementById('benefitsBtn').addEventListener('click', function() {
+    document.getElementById('infoText').innerHTML = 'Los beneficios de usar SourceTree incluyen una mayor claridad visual de los proyectos, facilidad de uso para principiantes y potentes herramientas para el manejo avanzado de versiones.';
+});
+
+document.getElementById('faqBtn').addEventListener('click', function() {
+    document.getElementById('infoText').innerHTML = 'Algunas dudas frecuentes incluyen cómo revertir cambios, cómo sincronizar forks, y cómo gestionar conflictos de merge.';
+});
